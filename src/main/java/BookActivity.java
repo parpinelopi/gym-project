b90490245c03f2b483b43757c58b6ed1003ecc52
Pinelopi Paraskevopoulou <pinelopiparaskevopoulou@Pinelopis-MBP.lan>
2021-01-06 13:34:26 +0100

    initialized variables for booking spot

--- src/main/java/BookActivity.java
+++ src/main/java/BookActivity.java
@@ -1,2 +1,9 @@
 public class BookActivity {
+
+    String activityInput;
+    String activityPositionAero;
+    String activityPositionSpin;
+    String activityPositionYoga;
+    InterfaceGym interGym = new InterfaceGym();
 }
+
