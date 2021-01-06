485aa7cb515fcd658ac595f16e0bd765ae868e24
Pinelopi Paraskevopoulou <pinelopiparaskevopoulou@Pinelopis-MBP.lan>
2021-01-06 13:10:39 +0100

    added method calculate cost

--- src/main/java/BecomeMember.java
+++ src/main/java/BecomeMember.java
@@ -13,4 +13,31 @@ public class BecomeMember {
     int durationD = 250; //Längre än 12 månader – 250 SEK/månad
     int totalCost;
     int months;
+
+
+    //calculate total membership cost
+    public void calculateCost() {
+        Scanner enterMonths = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("How many months will you become a member for? ");
+
+        months = enterMonths.nextInt();
+        enterMonths.close();
+        if (months >= 1 && months <= 2) {
+            totalCost = membership + durationA;
+            System.out.println("You have chosen membership between 1-2 months, this will cost " + totalCost);
+        }
+        // if statements for choosing membership according to months and cost calculation
+        else if (months >= 3 && months <= 6) {
+            totalCost = membership + durationB;
+            System.out.println("You have chosen membership between 3-6 months, this will cost " + totalCost);
+        } else if (months >= 7 && months <= 12) {
+            totalCost = membership + durationC;
+            System.out.println("You have chosen membership between 7-12 months, this will cost " + totalCost);
+        } else if (12 < months && months < 36) {
+            totalCost = membership + durationD;
+            System.out.println("You have chosen membership for over 12 months, this will cost " + totalCost);
+        } else
+            System.out.println("The number of months is invalid, please try again");
+    }
 }
+
