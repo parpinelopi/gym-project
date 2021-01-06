a1f3d08884c18ccc0ab4852b2b699181de3514bb
Pinelopi Paraskevopoulou <pinelopiparaskevopoulou@Pinelopis-MacBook-Pro.local>
2021-01-06 10:23:03 +0100

    initialiazed variables for membership

--- src/main/java/BecomeMember.java
+++ src/main/java/BecomeMember.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Class BecomeMember stores the functionality for when the user needs to choose a membership .
+ */
+
 public class BecomeMember {
+
+    int membership = 100;
+    int durationA = 400; //1-2 månader – 400 SEK/månad
+    int durationB = 350; //3-6 månader – 350 SEK/månad
+    int durationC = 300; //7-12 månader – 300 SEK/månad
+    int durationD = 250; //Längre än 12 månader – 250 SEK/månad
+    int totalCost;
+    int months;
 }
